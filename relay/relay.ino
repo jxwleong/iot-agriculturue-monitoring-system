7d03fd7227a6005617f15c4a09fd992735bb86be
jason9829 <jason9829@live.com>
2019-07-22 08:52:11 +0800

    added some comments for relay.ino

--- relay/relay.ino
+++ relay/relay.ino
@@ -1,9 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
+  // configure the GPIO pin
   pinMode(5, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // set the GPIO pin to HIGH or LOW to turn on/
+  // off the relay
   digitalWrite(5, HIGH);
 }
