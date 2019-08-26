0644bd098dd9e68108b4e94899277a2b62f970f0
jason9829 <jason9829@live.com>
2019-08-26 23:41:47 +0800

    Successfully send command/ data from rpc remote shell widget at ThingsBoard dashboard to ESP8266 device

--- ThingsBoard/sendRPC/rpc_ESP8266.ino
+++ ThingsBoard/sendRPC/rpc_ESP8266.ino
@@ -3,6 +3,8 @@
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 #include <ESP8266WiFi.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define WIFI_AP "xxxx"
 #define WIFI_PASSWORD "xxxx"
@@ -10,9 +12,11 @@
 #define TOKEN "xxxx"
 
 #define GPIO0 D2
+#define GPIO1 D3
 #define GPIO2 D1
 
 #define GPIO0_PIN D2
+#define GPIO1_PIN D3
 #define GPIO2_PIN D1
 
 char thingsboardServer[] = "demo.thingsboard.io";
@@ -24,12 +28,14 @@ PubSubClient client(wifiClient);
 int status = WL_IDLE_STATUS;
 
 // We assume that all GPIOs are LOW
-boolean gpioState[] = {false, false};
+boolean gpioState[] = {false, false, false};
+
 
 void setup() {
   Serial.begin(115200);
   // Set output mode for all GPIO pins
   pinMode(GPIO0, OUTPUT);
+  pinMode(GPIO1, OUTPUT);
   pinMode(GPIO2, OUTPUT);
   delay(10);
   InitWiFi();
@@ -45,20 +51,61 @@ void loop() {
   client.loop();
 }
 
+// bypass the json string and get command type in rpc remote shell
+char *getRpcCommandInStr(char *str, int bypassLength){
+  while(bypassLength != 0){
+    str++;
+    bypassLength--;
+  }
+  return str;
+}
+
+// expect input string already bypass previous characters to command by
+// calling getRpcCommandInStr
+// eg "123"}}
+int getRpcValInInt(char *str){
+  char *number;
+  long rpcVal = 0;
+  str++; // remove '"' character
+  rpcVal = strtol(str, NULL, 10);
+  return rpcVal;
+  }
+
 // The callback for when a PUBLISH message is received from the server.
 void on_message(const char* topic, byte* payload, unsigned int length) {
 
-  Serial.println("On message");
+  Serial.println("\nMessage from server received.");
 
   char json[length + 1];
+  char *data;
+  long dataInInt;
   strncpy (json, (char*)payload, length);
   json[length] = '\0';
 
-  Serial.print("Topic: ");
-  Serial.println(topic);
-  Serial.print("Message: ");
+  //Serial.print("Topic: ");
+  //Serial.println(topic);
+  //Serial.print("Message: ");
+  Serial.print("json: ");
   Serial.println(json);
+  // example of json received from rpc remote shell
+  // {"method":"sendCommand","params":{"command":"1"}}
+  // command are the variable that type in the rpc remote shell
+  // since json is in string, we can bypass it by 44 characters to gt
+  // "1"
+  data = getRpcCommandInStr(json, 44);
+  if(strstr(data, "turn on led")){
+    digitalWrite(GPIO2_PIN, 1);
+    }
+  else if(strstr(data, "turn off led")){
+    digitalWrite(GPIO2_PIN, 0);
+    }
+  else{
+  dataInInt = getRpcValInInt(data);
+  Serial.println("\n Data received in int:");
+  Serial.println(dataInInt);
+  }
 
+/*
   // Decode JSON request
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& data = jsonBuffer.parseObject((char*)json);
@@ -84,9 +131,9 @@ void on_message(const char* topic, byte* payload, unsigned int length) {
     responseTopic.replace("request", "response");
     client.publish(responseTopic.c_str(), get_gpio_status().c_str());
     client.publish("v1/devices/me/attributes", get_gpio_status().c_str());
-  }
+  }*/
 }
-
+/*
 String get_gpio_status() {
   // Prepare gpios JSON payload string
   StaticJsonBuffer<200> jsonBuffer;
@@ -112,9 +159,14 @@ void set_gpio_status(int pin, boolean enabled) {
     digitalWrite(GPIO2, enabled ? HIGH : LOW);
     // Update GPIOs state
     gpioState[1] = enabled;
+  }else if (pin == GPIO1_PIN) {
+    // Output GPIOs state
+    digitalWrite(GPIO1, enabled ? HIGH : LOW);
+    // Update GPIOs state
+    gpioState[2] = enabled;
   }
 }
-
+*/
 void InitWiFi() {
   Serial.println("Connecting to AP ...");
   // attempt to connect to WiFi network
@@ -147,8 +199,8 @@ void reconnect() {
       // Subscribing to receive RPC requests
       client.subscribe("v1/devices/me/rpc/request/+");
       // Sending current GPIO status
-      Serial.println("Sending current GPIO status ...");
-      client.publish("v1/devices/me/attributes", get_gpio_status().c_str());
+     // Serial.println("Sending current GPIO status ...");
+      //client.publish("v1/devices/me/attributes", get_gpio_status().c_str());
     } else {
       Serial.print( "[FAILED] [ rc = " );
       Serial.print( client.state() );
